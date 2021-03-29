4c8f4ee5dd84631d7ed1a0461a317fa8aec4e866
tyn@Manjaro <1589972754@qq.com>
2021-03-29 19:56:58 +0800

    Add lines into fish configure files to enable pyenv & pyenv-virtualenv.

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,10 @@
 # Default editor
 set -x EDITOR vim
+
+# pyenv init
+if command -v pyenv 1>/dev/null 2>&1
+  pyenv init - | source
+end
+# pyenv-virtualenv init
+status --is-interactive; and pyenv init - | source
+status --is-interactive; and pyenv virtualenv-init - | source

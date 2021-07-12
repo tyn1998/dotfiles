f2536fc28b6915c4aad8c5013a118f74e7234def
tyn@Manjaro <1589972754@qq.com>
2021-07-12 13:52:53 +0800

    fish: disable greeting & update pyenv init.

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,9 @@
 # Default editor
 set -x EDITOR vim
 
-# pyenv init
-if command -v pyenv 1>/dev/null 2>&1
-  pyenv init - | source
-end
-# pyenv-virtualenv init
-status --is-interactive; and pyenv init - | source
-status --is-interactive; and pyenv virtualenv-init - | source
+# Disable fish welcome message
+set fish_greeting
+
+# pyenv init for fish
+status is-interactive; and pyenv init --path | source
+pyenv init - | source
